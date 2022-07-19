7172f5d9567fc1af7e82367b4169515009dfc113
BKTales <47539420+BKTales@users.noreply.github.com>
2022-07-19 12:21:37 +0100

    Update

--- Cart.java
+++ Cart.java
@@ -2,23 +2,24 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-public class Cart {
+public class Cart extends CartStock{
 
     Scanner scan = new Scanner(System.in);
-    private int cartId;
-    private Map<Integer, Float> itemsHashMap = new HashMap<>();
-    private float totalCost;
+    public int cartId;
+    public Map<Integer, Float> itemsHashMap;
+    public float totalCost;
 
     public Cart(int cartId, Map itemsHashMap, float totalCost) {
         this.cartId = cartId;
         this.itemsHashMap = itemsHashMap;
         this.totalCost = totalCost;
     }
+
     public void createItem(){
         System.out.println("Input itemId and itemCost: \n");
         itemsHashMap.put(scan.nextInt(), scan.nextFloat());
     }
-    private void addItemToCart(){
+    public void addItemToCart(){
         System.out.println("Input itemId, price and cartId");
         itemsHashMap.put(scan.nextInt(), scan.nextFloat());
         cartId = scan.nextInt();
@@ -28,5 +29,41 @@ public class Cart {
             totalCost += value;
         }
         return totalCost;
+    }
+    public void menu(){
+        char option = ' ';
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Select a command: ");
+        System.out.println("a) Item <itemId> <cost>");
+        System.out.println("b) Create a cart <cartId>");
+        System.out.println("c) Add <itemId> <cartId>");
+        System.out.println("d) Pay <cartId>");
+        System.out.println("e) Exit");
+
+        do{
+            System.out.println("Choose an option: ");
+            option = scan.next().charAt(0);
+            System.out.println("\n");
+
+            switch (option){
+                case 'a':
+                    createItem();
+                    break;
+                case 'b':
+                    addCart(cartId, itemsHashMap, totalCost);
+                    break;
+                case 'c':
+                    addItemToCart();
+                    break;
+                case 'd':
+                    totalCost();
+                    break;
+                case 'e':
+                    System.exit(1);
+                default:
+                    System.out.println("Please choose a valid command");
+                    break;
+            }
+        }while (option != 'e');
     }
 }
--- MarketSystem.java
+++ MarketSystem.java
@@ -1,10 +1,15 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class MarketSystem {
     public static void main(String[] args) {
+        int cartId = 0;
+        Map<Integer, Float> itemsHashMap = new HashMap<>();
+        float totalCost = 0f;
         Scanner scan = new Scanner(System.in);
-        System.out.println("Input 'Item ID' and 'Item Cost'");
-        int itemId = scan.nextInt();
-        float itemCost = scan.nextFloat();
+
+        Cart obj1 = new Cart(cartId, itemsHashMap, totalCost);
+        obj1.menu();
     }
 }
